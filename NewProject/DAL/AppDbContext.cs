a52e10950d903f26e4e18e9735530917134f2a48
nijat.gasimli <nijat.gasimli@CASR0>
2023-03-10 15:14:32 +0400

    Add project files.

---
+++ NewProject/DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewProject.Models;
+
+namespace NewProject.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext _context { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
